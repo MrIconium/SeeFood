7d69042b1fdf6bba355b21ac6b60e6f56d543f8c
MrIconium <mehmetemrahk@gmail.com>
2024-11-30 10:48:48 +0300

    Initial Commit

---
+++ SeeFoodTests/SeeFoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodTests.swift
+//  SeeFoodTests
+//
+//  Created by Mehmet Emrah Konya on 30.11.2024.
+//
+
+import Testing
+@testable import SeeFood
+
+struct SeeFoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
